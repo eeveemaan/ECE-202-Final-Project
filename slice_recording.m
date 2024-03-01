8ab6a124532ddf35e31a649d29b4cfe7bc07b88b
GitShanks14 <=>
2024-02-29 17:11:36 -0800

    inclusion of spectrogram

--- slice_recording.m
+++ slice_recording.m
@@ -5,7 +5,7 @@ load(strcat(fpath,fname));
 
 %% Quick filter
 Fs = 5000;
-Tsnip = 0.1; N = Fs*Tsnip;
+Tsnip = 2; N = Fs*Tsnip;
 t = 0:1/Fs:Tsnip;
 lsave = length(savedata(1,:));
 %data_f = fft(savedata(1,:)); data_f(50/Fs*lsave+1:end-50/Fs*lsave)=0;
@@ -30,34 +30,34 @@ idx_sound = find(savesound); L=length(idx_sound);
 
 snippets_d = zeros(length(idx_sound),N+1);
 
-figure;
+% figure;
 for ii=1:L
     snippets_d(ii,:)=data_filt(1,idx_sound(ii)-N/2:idx_sound(ii)+N/2);
     %snippets_d(ii,:)=data_ds(1,idx_sound(ii)-N/2:idx_sound(ii)+N/2);
-    subplot(L,1,ii)
-    plot(t,snippets_d(ii,:))
+    % subplot(L,1,ii)
+    % plot(t,snippets_d(ii,:))
 end
 
 figure(pos=[0 1000 600 300]);
 plot(t*1e3,mean(snippets_d,1),color=[0.2 0.5 0.9 1],LineWidth=2); hold on;
-plot(t*1e3,snippets_d,color=[0.2 0.5 0.9 0.3])
-rectangle('Position',[50 -25 18 45],'FaceColor',[0.6 0.6 0.2 0.1],'EdgeColor',[0.6 0.6 0.1 0.3]);
-text(55,-20,"Sound","FontSize",12,"FontWeight",'bold',"FontName",'Arial')
+plot(t*1e3,snippets_d,color=[0.2 0.5 0.9 0.01])
 ax = gca; ax.LineWidth = 2; ax.FontName = 'Arial'; ax.FontSize = 12;
+rectangle('Position',[Tsnip/2*1000 ax.YLim(1) 18 ax.YLim(2)-ax.YLim(1)],'FaceColor',[0.6 0.6 0.2 0.1],'EdgeColor',[0.6 0.6 0.1 0.3]);
+text(Tsnip/2*1000+5,0.8*ax.YLim(1),"Sound","FontSize",12,"FontWeight",'bold',"FontName",'Arial')
 title('sound centered epochs overlayed'); xlabel('Time (ms)'); ylabel('Potential (\muV)');
 legend('Average','Individual')
 
 
 %% FFT of snippets
 snippets_f = zeros(length(idx_sound),N+1);
-figure;
+% figure;
 f = 0:Fs/N:Fs;
 for ii=1:L
     snippets_f(ii,:) = fft(snippets_d(ii,:));
     snippets_f(ii,50:end-50) = 0;
-    subplot(L,1,ii);
-    plot(f,abs(snippets_f(ii,:)))
-    xlim([0 50])
+    % subplot(L,1,ii);
+    % plot(f,abs(snippets_f(ii,:)))
+    % xlim([0 50])
 end
 
 
@@ -75,4 +75,25 @@ end
 %% Welch
 %pwelch(data_ds(1,:),N,0,N,Fs,"onesided","psd")
 pwelch(data_filt(1,:),N,0,N,Fs,"onesided","psd")
-%xlim([0 .050])
+xlim([0 .050])
+
+%% Spectrogram
+data_f = fft(savedata(1,:)); data_f(50/Fs*lsave+1:end-50/Fs*lsave)=0;
+data_filt = real(ifft(data_f));
+
+% Downsampling
+data_ds = data_filt(1,1:50:end);
+idx_ds = int32(round(idx_sound/50,0));
+
+% Fs original: 5000, downsample => 250 (if / 20), => 100 (if / 50)
+Fs = 100;
+Tsnip = 0.1; N = Fs*Tsnip;
+t = 0:1/Fs:Tsnip;
+
+figure; 
+[~,~,~,ps] = spectrogram(data_ds,100,50,200,100); 
+spectrogram(data_ds,100,50,200,100); 
+view(90,-90); colormap('jet'); caxis([-5 25]);
+c=colorbar; ylabel(c,'Power/frequency (dB/Hz)')
+hold on;
+plot(0.1*ones(length(idx_sound),1),savetime(idx_sound)/60,linewidth=5,Marker='|')
