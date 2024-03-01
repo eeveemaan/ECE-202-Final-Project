b030d46cfa67b79c5e4fdcb92b3265f38d542d4f
eeveemaan <72529767+eeveemaan@users.noreply.github.com>
2024-03-01 12:31:34 -0800

    Update slice_recording.m
    - Created code block "%%Variables" and added variable "after"
      - after: variable between [0,1] inclusive reflecting percentage of epoch (time window snippet) after sound is played
    - also made figure/plot parameter syntax adjustments so it would run on my version of matlab

--- slice_recording.m
+++ slice_recording.m
@@ -2,6 +2,8 @@
 [fname,fpath] = uigetfile("./data/*.mat");
 load(strcat(fpath,fname));
 
+%% Variables
+after = 0.5;    % [0,1] fraction/percentage of epoch window that is after sound played
 
 %% Quick filter
 Fs = 5000;
@@ -37,6 +39,7 @@ snippets_d = zeros(length(idx_sound),N+1,2);
 % figure;
 for ii=1:L
     for jj=1:2
+%         snippets_d(ii,:,jj)=data_filt(jj,idx_sound(ii)-round(N*(1-after),0):idx_sound(ii)+round(N*after,0));
         snippets_d(ii,:,jj)=data_filt(jj,idx_sound(ii)-N/2:idx_sound(ii)+N/2);
         %snippets_d(ii,:)=data_ds(1,idx_sound(ii)-N/2:idx_sound(ii)+N/2);
         % subplot(L,1,ii)
@@ -45,21 +48,21 @@ for ii=1:L
 end
 
 %% Plot overlay
-figure(pos=[0 1000 600 600]);
+figure('Position',[0 10 600 600]);
 subplot(2,1,1);
-plot(t*1e3,mean(snippets_d(:,:,1),1),color=[0.2 0.5 0.9 1],LineWidth=2); hold on;
-plot(t*1e3,snippets_d(:,:,1),color=[0.2 0.5 0.9 0.1])
+plot(t*1e3,mean(snippets_d(:,:,1),1),'Color',[0.2 0.5 0.9 1],'LineWidth',2); hold on;
+plot(t*1e3,snippets_d(:,:,1),'Color',[0.2 0.5 0.9 0.1])
 ax = gca; ax.LineWidth = 2; ax.FontName = 'Arial'; ax.FontSize = 12;
 text(Tsnip/2*1000+1,0.8*ax.YLim(1),"Sound","FontSize",8,"FontWeight",'bold',"FontName",'Arial')
-rectangle('Position',[Tsnip/2*1000 ax.YLim(1) 18 ax.YLim(2)-ax.YLim(1)],'FaceColor',[0.6 0.6 0.2 0.1],'EdgeColor',[0.6 0.6 0.1 0.3]);
+rectangle('Position',[Tsnip*(1-after)*1000 ax.YLim(1) 18 ax.YLim(2)-ax.YLim(1)],'FaceColor',[0.6 0.6 0.2 0.1],'EdgeColor',[0.6 0.6 0.1 0.3]);
 title('Channel 19'); xlabel('Time (ms)'); ylabel('Potential (\muV)');
 legend('Average','Individual')
 
 subplot(2,1,2);
-plot(t*1e3,mean(snippets_d(:,:,1),1),color=[0.2 0.5 0.9 1],LineWidth=2); hold on;
-plot(t*1e3,snippets_d(:,:,1),color=[0.2 0.5 0.9 0.1])
+plot(t*1e3,mean(snippets_d(:,:,1),1),'Color',[0.2 0.5 0.9 1],'LineWidth',2); hold on;
+plot(t*1e3,snippets_d(:,:,1),'Color',[0.2 0.5 0.9 0.1])
 ax = gca; ax.LineWidth = 2; ax.FontName = 'Arial'; ax.FontSize = 12;
-rectangle('Position',[Tsnip/2*1000 ax.YLim(1) 18 ax.YLim(2)-ax.YLim(1)],'FaceColor',[0.6 0.6 0.2 0.1],'EdgeColor',[0.6 0.6 0.1 0.3]);
+rectangle('Position',[Tsnip*(1-after)*1000 ax.YLim(1) 18 ax.YLim(2)-ax.YLim(1)],'FaceColor',[0.6 0.6 0.2 0.1],'EdgeColor',[0.6 0.6 0.1 0.3]);
 text(Tsnip/2*1000+1,0.8*ax.YLim(1),"Sound","FontSize",8,"FontWeight",'bold',"FontName",'Arial')
 title('Channel 20'); xlabel('Time (ms)'); ylabel('Potential (\muV)');
 legend('Average','Individual')
@@ -117,7 +120,7 @@ Fs = Fs/D;
 Tsnip = 0.1; N = Fs*Tsnip;
 t = 0:1/Fs:Tsnip;
 
-figure(pos=[1000 1000 1000 1000]);
+figure('Position',[1000 1000 1000 1000]);
 subplot(1,2,1)
 [~,~,~,ps] = spectrogram(data_ds(2,:),100,50,200,100);
 spectrogram(data_ds(2,:),100,50,200,100); 
