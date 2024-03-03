b26e3fa20d518d8d4957eaf2a5c74c1e943bd59c
GitShanks14 <=>
2024-03-02 18:13:24 -0800

    test

--- slice_recording.m
+++ slice_recording.m
@@ -26,6 +26,8 @@ plot(savetime(discard_sf:end-discard_sf),data_filt(2,discard_sf:end-discard_sf))
 %% Split out snippets
 idx_sound = find(savesound); L=length(idx_sound);
 
+lcount=sum(savesound==1); rcount=sum(savesound==2); scount=sum(savesound==3);
+
 % % Downsampling
 % data_ds = data_filt(1,1:20:end);
 % idx_sound = int32(round(idx_sound/20,0));
@@ -36,23 +38,38 @@ idx_sound = find(savesound); L=length(idx_sound);
 % t = 0:1/Fs:Tsnip;
 
 snippets_d = zeros(length(idx_sound),N+1,2);
+snippets_l = zeros(lcount,N+1,2); lcounter=1;
+snippets_r = zeros(rcount,N+1,2); rcounter=1;
+snippets_s = zeros(scount,N+1,2); scounter=1;
 
 % figure;
 for ii=1:L
     for jj=1:2
         snippets_d(ii,:,jj)=data_filt(jj,idx_sound(ii)-round(N*(1-after),0):idx_sound(ii)+round(N*after,0));
+
         %snippets_d(ii,:,jj)=data_filt(jj,idx_sound(ii)-N/2:idx_sound(ii)+N/2);
         %snippets_d(ii,:)=data_ds(1,idx_sound(ii)-N/2:idx_sound(ii)+N/2);
         % subplot(L,1,ii)
         % plot(t,snippets_d(ii,:))
+    end
+
+    if(savesound(idx_sound(ii))==1)
+        snippets_l(lcounter,:,:)=snippets_d(ii,:,:);
+        lcounter=lcounter+1;
+    elseif(savesound(idx_sound(ii))==2)
+        snippets_r(rcounter,:,:)=snippets_d(ii,:,:);
+        rcounter=rcounter+1;
+    elseif(savesound(idx_sound(ii))==3)
+        snippets_s(scounter,:,:)=snippets_d(ii,:,:);
+        scounter=scounter+1;
     end
 end
 
 % Plot overlay
 figure('Position',[0 10 600 600]);
 subplot(2,1,1);
-plot(t*1e3,mean(snippets_d(:,:,1),1),'Color',[0.2 0.5 0.9 1],'LineWidth',2); hold on;
-plot(t*1e3,snippets_d(:,:,1),'Color',[0.2 0.5 0.9 0.1])
+plot(t*1e3,mean(snippets_l(:,:,1),1),'Color',[0.2 0.5 0.9 1],'LineWidth',2); hold on;
+plot(t*1e3,snippets_l(:,:,1),'Color',[0.2 0.5 0.9 0.1])
 ax = gca; ax.LineWidth = 2; ax.FontName = 'Arial'; ax.FontSize = 12;
 text(Tsnip/2*1000+1,0.8*ax.YLim(1),"Sound","FontSize",8,"FontWeight",'bold',"FontName",'Arial')
 rectangle('Position',[Tsnip*(1-after)*1000 ax.YLim(1) 18 ax.YLim(2)-ax.YLim(1)],'FaceColor',[0.6 0.6 0.2 0.1],'EdgeColor',[0.6 0.6 0.1 0.3]);
@@ -60,8 +77,8 @@ title('Channel 19'); xlabel('Time (ms)'); ylabel('Potential (\muV)');
 legend('Average','Individual')
 
 subplot(2,1,2);
-plot(t*1e3,mean(snippets_d(:,:,1),1),'Color',[0.2 0.5 0.9 1],'LineWidth',2); hold on;
-plot(t*1e3,snippets_d(:,:,1),'Color',[0.2 0.5 0.9 0.1])
+plot(t*1e3,mean(snippets_l(:,:,1),1),'Color',[0.2 0.5 0.9 1],'LineWidth',2); hold on;
+plot(t*1e3,snippets_l(:,:,1),'Color',[0.2 0.5 0.9 0.1])
 ax = gca; ax.LineWidth = 2; ax.FontName = 'Arial'; ax.FontSize = 12;
 rectangle('Position',[Tsnip*(1-after)*1000 ax.YLim(1) 18 ax.YLim(2)-ax.YLim(1)],'FaceColor',[0.6 0.6 0.2 0.1],'EdgeColor',[0.6 0.6 0.1 0.3]);
 text(Tsnip/2*1000+1,0.8*ax.YLim(1),"Sound","FontSize",8,"FontWeight",'bold',"FontName",'Arial')
